716ecfdd3ae217c1674dbe73d75cfbc8d0a1f97d
bilal-uforia <bilalmalik@uforiainfotech.com>
2024-04-03 14:33:19 +0500

    cls cls cls cls cls cls cls iframe message listner added

--- src/components/Toolbar/index.jsx
+++ src/components/Toolbar/index.jsx
@@ -5,7 +5,7 @@ const Toolbar = () => {
         <div className="w-[68px] bg-[#fff] rounded-[12px] border-[#e0e0e0] flex flex-col items-center min-h-[500px] p-1">
             <div className="flex flex-col items-center gap-2 hover:bg-[#ECEFFD] w-full rounded-[12px] cursor-pointer">
                 <svg role="img" className="icon icon-mono icon-image w-5 h-5 text-current">
-                    <use xlink:href="https://app-dev.glorify.com/assets/designer/static/media/icons-mono.svg?v=321#icon-image"></use>
+                    <use xlinkHref="https://app-dev.glorify.com/assets/designer/static/media/icons-mono.svg?v=321#icon-image"></use>
                 </svg>
                 <p>Images</p>
             </div>
