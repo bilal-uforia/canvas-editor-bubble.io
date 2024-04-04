135a2e29675537d6325203666991e14d655db48b
bilal-uforia <bilalmalik@uforiainfotech.com>
2024-04-04 16:00:05 +0500

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,9 +38,15 @@ function App() {
                 const response = await axios.get(`/a_contentasset/${fileId}`);
                 console.log("File Data is: ", response?.data?.response);
                 setFileData(response?.data?.response);
-                console.log("Page List is : ", response?.data?.response?.a2_page_list_list_custom_scene);
+                const A_Page_List = response?.data?.response?.a2_page_list_list_custom_scene
+                console.log("Page List is : ", A_Page_List);
 
-                //showing A_FW Page
+                //showing A_FW Pages
+                A_Page_List.map(async(page_id,index)=>{
+                    const response = await axios.get(`/a_page(fw)/${page_id}`);
+                    console.log(`Page ${index+1} Data is: `, response?.data?.response);
+                    console.log("I-Canvas JSON is: ", response?.data?.response?.i_canvas_json_text);
+                });
 
             })();
 
