2b76339b5f9412bc633ff144047636aac64b8ffb
bilal-uforia <bilalmalik@uforiainfotech.com>
2024-04-04 14:41:37 +0500

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useMemo, useEffect} from "react";
 import CanvasProvider from "./ContextProviders/CanvasProvider";
 import CanvasRenderer from "./components/Canvas/CanvasRenderer";
 import Header from "./components/Header";
@@ -6,30 +6,40 @@ import Toolbar from "./components/Toolbar";
 import InspectPanel from "./components/InspectPanel";
 
 function App() {
+    const [userId, setUserId] = useState(null);
+    const [fileId, setFileId] = useState(null);
 
-    window.addEventListener('message', function(event) {
-        if (event.origin !== "https://hookbook.io") {
-            return;
-        }
-        switch(event.data.type){
-            case "user file id":
-                console.log("Event data: ", event.data, event.origin);
-                console.log("Message received from the parent: " + event.data.message);
-                break;
-            case "current user id":
-                console.log("Current user id is: ", event.data.message)
-        }
+    useMemo(() => {
+        window.addEventListener('message', function (event) {
+            if (event.origin !== "https://hookbook.io") {
+                return;
+            }
+            switch (event.data.type) {
+                case "user file id":
+                    console.log("Event data: ", event.data, event.origin);
+                    console.log("Message received from the parent: " + event.data.message);
+                    setFileId(event.data.message);
+                    break;
+                case "current user id":
+                    console.log("Current user id is: ", event.data.message);
+                    setUserId(event.data.message)
+                    break;
+            }
 
-    });
+        });
+    }, []);
+
+
+
 
 
     return (
         <CanvasProvider>
             <div>
                 <Header/>
-                <CanvasRenderer />
+                <CanvasRenderer/>
                 <Toolbar/>
-                <InspectPanel />
+                <InspectPanel/>
             </div>
         </CanvasProvider>
     )
