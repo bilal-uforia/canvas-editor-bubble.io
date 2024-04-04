af2d618cd5139b92a17fe0af1a9cab5b938ca303
bilal-uforia <bilalmalik@uforiainfotech.com>
2024-04-04 14:23:10 +0500

    shifter message event to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,22 @@ import InspectPanel from "./components/InspectPanel";
 
 function App() {
 
+    window.addEventListener('message', function(event) {
+        if (event.origin !== "https://hookbook.io") {
+            return;
+        }
+        switch(event.data.type){
+            case "user file id":
+                console.log("Event data: ", event.data, event.origin);
+                console.log("Message received from the parent: " + event.data.message);
+                break;
+            case "current user id":
+                console.log("Current user id is: ", event.data.message)
+        }
+
+    });
+
+
     return (
         <CanvasProvider>
             <div>
